92406652af68a54e284e6147bda601ee4491b1c2
pat pataranutaporn <patpataranutaporn@dhcp-18-30-22-180.dyn.mit.edu>
2019-01-06 15:55:49 -0500

    Update GPS demo with map zooming in

--- GPS demo/GPS demo/ViewController.swift
+++ GPS demo/GPS demo/ViewController.swift
@@ -1,69 +1,47 @@
-//
-//  ViewController.swift
-//  GPS demo
-//
-//  Created by pat pataranutaporn on 1/6/19.
-//  Copyright © 2019 pat pataranutaporn. All rights reserved.
-//
-
 import UIKit
+import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    let locationManager = CLLocationManager()
-
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var LocateMe_button: UIButton!
+    
+    var locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationManager.requestAlwaysAuthorization()
-        
-        // For use when the app is open
-        //locationManager.requestWhenInUseAuthorization()
-        
-        // If location services is enabled get the users location
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
-            locationManager.startUpdatingLocation()
-        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     
+    @IBAction func locateMe(_ sender: Any) {
+        print("Hello I was touched like a virgin")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+    }
     
-    // Print out the location to the console
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print(location.coordinate)
-        }
-    }
-    
-    // If we have been deined access give the user the option to change it
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if(status == CLAuthorizationStatus.denied) {
-            showLocationDisabledPopUp()
-        }
-    }
-    
-    // Show the popup to the user if we have been deined access
-    func showLocationDisabledPopUp() {
-        let alertController = UIAlertController(title: "Background Location Access Disabled",
-                                                message: "In order to deliver pizza we need your location",
-                                                preferredStyle: .alert)
+        let userLocation:CLLocation = locations[0] as CLLocation
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alertController.addAction(cancelAction)
+        manager.stopUpdatingLocation()
         
-        let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
-            if let url = URL(string: UIApplication.openSettingsURLString) {
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-        }
-        alertController.addAction(openAction)
+        let coordinations = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2,longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordinations, span: span)
         
-        self.present(alertController, animated: true, completion: nil)
+        mapView.setRegion(region, animated: true)
+        
+       
     }
-
-
 }
-
