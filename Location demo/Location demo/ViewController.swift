b884d0e4a74213a520566fa5126c81c257b1db0d
pat pataranutaporn <patpataranutaporn@dhcp-18-30-22-180.dyn.mit.edu>
2019-01-06 13:12:59 -0500

    Add location demo

---
+++ Location demo/Location demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location demo
+//
+//  Created by pat pataranutaporn on 1/6/19.
+//  Copyright © 2019 pat pataranutaporn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
