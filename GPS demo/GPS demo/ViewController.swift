aa47fbe9393768bdde79f8e47f3e3716be2c3b3c
pat pataranutaporn <patpataranutaporn@dhcp-18-30-22-180.dyn.mit.edu>
2019-01-06 16:53:28 -0500

    Update GPS with location decoder

--- GPS demo/GPS demo/ViewController.swift
+++ GPS demo/GPS demo/ViewController.swift
@@ -6,6 +6,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var LocateMe_button: UIButton!
+
+    @IBOutlet weak var coordination_label: UILabel!
+    @IBOutlet weak var location_label: UILabel!
     
     var locationManager = CLLocationManager()
     
@@ -22,7 +25,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
     @IBAction func locateMe(_ sender: Any) {
-        print("Hello I was touched like a virgin")
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
@@ -40,8 +42,60 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let span = MKCoordinateSpan(latitudeDelta: 0.2,longitudeDelta: 0.2)
         let region = MKCoordinateRegion(center: coordinations, span: span)
         
+        let lat = userLocation.coordinate.latitude
+        let long = userLocation.coordinate.longitude
+        
         mapView.setRegion(region, animated: true)
+        coordination_label.text = String(lat) + "," + String(long)
+        
+        var geocoder = CLGeocoder()
+        // Create Location
+        let location = CLLocation(latitude: lat, longitude: long)
+        
+        // Geocode Location
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if let error = error {
+                print("Unable to Reverse Geocode Location (\(error))")
+                self.location_label.text = "Unable to Find Address for Location"
+                
+            } else {
+                if let placemarks = placemarks, let placemark = placemarks.first {
+                    self.location_label.text = placemark.compactAddress
+                } else {
+                    self.location_label.text = "No Matching Addresses Found"
+                }
+            }
+        
+        }
         
        
     }
 }
+
+extension CLPlacemark {
+    
+    var compactAddress: String? {
+        if let name = name {
+            var result = name
+            
+            if let street = thoroughfare {
+                result += ", \(street)"
+            }
+            
+            if let city = locality {
+                result += ", \(city)"
+            }
+            
+            if let country = country {
+                result += ", \(country)"
+            }
+            
+            return result
+        }
+        
+        return nil
+    }
+    
+}
+
+
