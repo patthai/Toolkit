f6542b65faa69cedf46141f4d974e05167690b3d
pat pataranutaporn <patpataranutaporn@pats-MacBook-Pro-2.local>
2019-05-09 03:57:29 -0400

    recording app

--- Record_IMU/Record_IMU/ViewController.swift
+++ Record_IMU/Record_IMU/ViewController.swift
@@ -15,13 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var record_button: UIButton!
     @IBOutlet weak var reset_button: UIButton!
     @IBOutlet weak var record_duration: UITextField!
-    @IBOutlet weak var timer_slider: UISlider!
     @IBOutlet weak var timer: UILabel!
-    @IBOutlet weak var sampling_rate: UITextField!
+    @IBOutlet weak var sampling_rate: UISegmentedControl!
     
+    @IBOutlet weak var output: UITextView!
     var record_status = false
-    var sampling_rate_value: Double = 0.5
-    var record_duration_value: Double = 100
+    var sampling_rate_value = 0.5
+    
+    var record_time = 0
+    var record_duration_value = 100
+    @IBOutlet weak var progress: UIProgressView!
+    
     var record_IMU_data : [[Double]] = []
     let motionManager = CMMotionManager()
     
@@ -36,35 +40,48 @@ class ViewController: UIViewController {
             record_button.setTitle("Stop", for: [])
             record_status = true
             update_values()
+            record_IMU_data = []
             start_motion_data()
+            
         }
             
         else {
-            record_button.setTitle("Record", for: [])
-            record_status = false
             stop_motion_data()
-            update_values()
+
         }
     }
     
     @IBAction func press_reset(_ sender: Any) {
     }
     
+    @IBAction func rate_button(_ sender: Any) {
+        switch sampling_rate.selectedSegmentIndex
+        {
+        case 0:
+            sampling_rate_value = 0.5
+        case 1:
+            sampling_rate_value = 0.1
+        case 2:
+            sampling_rate_value = 0.05
+        default:
+            break
+        }
+        update_values()
+    }
+    
+    
     func update_values()
     {
-        sampling_rate_value = Double(sampling_rate.text!) as! Double
-        record_duration_value = Double(record_duration.text!)as! Double
-        
-        sampling_rate.text = String(sampling_rate_value)
+        record_duration_value = Int(record_duration.text!) ?? 0
         record_duration.text = String(record_duration_value)
         print (record_status, record_duration_value, sampling_rate_value)
-        
     }
     
     
     
     func start_motion_data()
     {
+        var count = 0
         var uAX: Double = 0.0
         var uAY: Double = 0.0
         var uAZ: Double = 0.0
@@ -75,7 +92,7 @@ class ViewController: UIViewController {
         
         
         if motionManager.isDeviceMotionAvailable{
-            motionManager.deviceMotionUpdateInterval = 0.02
+            motionManager.deviceMotionUpdateInterval = sampling_rate_value
             motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { (data, error) in
                 // print(data)
                 
@@ -87,7 +104,17 @@ class ViewController: UIViewController {
                 uGX = data?.rotationRate.x ?? 0
                 uGY = data?.rotationRate.y ?? 0
                 uGZ = data?.rotationRate.z ?? 0
-                print (time, uAX, uAY, uAZ, uGX, uGY, uGZ)
+                //print (time, uAX, uAY, uAZ, uGX, uGY, uGZ)
+                self.record_IMU_data.append([time, uAX, uAY, uAZ, uGX, uGY, uGZ])
+                count = count + 1
+            
+                print(count, self.record_duration_value)
+                
+                if (count > self.record_duration_value)
+                {
+                    self.stop_motion_data()
+                }
+                
             }
         }
     }
@@ -98,6 +125,11 @@ class ViewController: UIViewController {
         if motionManager.isDeviceMotionAvailable{
             motionManager.stopDeviceMotionUpdates()
         }
+        self.record_button.setTitle("Record", for: [])
+        self.record_status = false
+        print (record_IMU_data)
+        update_values()
+
     }
     
 }
